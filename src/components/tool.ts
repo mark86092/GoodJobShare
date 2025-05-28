950fc00f7391bf6e9a5eb29977dc14b4331a69b5
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-05-28 10:29:18 +0000

    update

--- src/components/tool.ts
+++ src/components/tool.ts
@@ -1,23 +1,27 @@
 interface User {
   name: string;
-	id: number;
+  id: number;
 }
 
-
 const fetchUser = async (): Promise<User> => {
-  const response = await fetch("http://example.com");
+  const response = await fetch('http://example.com');
 
   const data = await response.json();
 
-  if (typeof data === 'object' && data !== null && 'name' in data && 'id' in data) {
+  if (
+    typeof data === 'object' &&
+    data !== null &&
+    'name' in data &&
+    'id' in data
+  ) {
     return data as User;
   }
 
-  throw new Error("error")
-}
+  throw new Error('error');
+};
 
 export default async function main(): Promise<void> {
   const user: User = await fetchUser();
 
-  console.log(user.id)
+  console.log(user.id);
 }
