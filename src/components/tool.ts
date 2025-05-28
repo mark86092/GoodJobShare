15485e56f77c3d7f5e86b978040a17fbd899fb4c
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-05-28 10:18:02 +0000

    try

---
+++ src/components/tool.ts
@@ -0,0 +1,23 @@
+interface User {
+  name: string;
+	id: number;
+}
+
+
+const fetchUser = async (): Promise<User> => {
+  const response = await fetch("http://example.com");
+
+  const data = await response.json();
+
+  if (typeof data === 'object' && data !== null && 'name' in data && 'id' in data) {
+    return data as User;
+  }
+
+  throw new Error("error")
+}
+
+export default async function main(): Promise<void> {
+  const user: User = await fetchUser();
+
+  console.log(user.id)
+}
