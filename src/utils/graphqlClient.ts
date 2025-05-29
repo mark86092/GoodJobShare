10cf7cd23f09bab5d4efefd590d0e62ebfe00930
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-05-29 07:51:44 +0000

    update

--- src/utils/graphqlClient.ts
+++ src/utils/graphqlClient.ts
@@ -1,51 +1,43 @@
 import 'isomorphic-fetch';
-
-import { stringify } from 'qs';
-
 import { HttpError } from 'utils/errors';
-
-import { API_HOST } from '../config';
 import { GraphqlError } from 'utils/errors';
-import fetchUtil from './fetchUtil';
-
-// const fetch = fetchUtil('/graphql');
 
 type GraphqlClientArgs = {
   query: string;
   variables?: unknown;
-  options?: unknown;
   token?: string;
 };
-
-const headerBuilder = token =>
-  token
-    ? {
-        'Content-Type': 'application/json',
-        Authorization: `Bearer ${token}`,
-      }
-    : {
-        'Content-Type': 'application/json',
-      };
-
-
 
 const graphqlClient = async <T>({
   variables,
   query,
-  options,
   token,
 }: GraphqlClientArgs): Promise<T> => {
-  fetch.post('/graphql', { headers: headerBuilder(token) });
-
-  const response = await fetch.post({
-    body: { query, variables },
-    token,
-    options,
-  });
-  if (response.errors) {
-    throw new GraphqlError(response.errors);
+  const headers = new Headers();
+  headers.append('Content-Type', 'application/json');
+  if (token) {
+    headers.append('Authorization', `Bearer ${token}`);
   }
-  return response.data;
+
+  const response = await fetch('/graphql', {
+    method: 'POST',
+    headers: headers,
+    body: JSON.stringify({ query, variables }),
+  });
+
+  if (!response.ok) {
+    const apiResponse = await response.json();
+    throw new HttpError(apiResponse.message, {
+      statusCode: response.status,
+    });
+  }
+
+  const apiResponse = await response.json();
+
+  if (apiResponse.errors) {
+    throw new GraphqlError(apiResponse.errors);
+  }
+  return apiResponse.data;
 };
 
 export default graphqlClient;
