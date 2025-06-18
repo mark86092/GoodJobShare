234b918f4ca467671b264799702c7ec6dff5ac69
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-06-18 12:57:28 +0000

    fix

--- src/hooks/login/useLogin.js
+++ src/hooks/login/useLogin.js
@@ -2,7 +2,7 @@ import { useCallback, useContext } from 'react';
 import { useIsLoggedIn } from 'hooks/auth';
 import LoginModalContext from 'contexts/LoginModalContext';
 
-const useLogin = (): [boolean, () => void] => {
+const useLogin = () => {
   const isLoggedIn = useIsLoggedIn();
   const { setLoginModalDisplayed } = useContext(LoginModalContext);
   const login = useCallback(() => setLoginModalDisplayed(true), [
