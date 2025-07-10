b51b47485bea056cf75d86d8d8f36edfe33a30f3
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-07-10 06:29:57 +0000

    fix

--- src/routes.js
+++ src/routes.js
@@ -22,12 +22,12 @@ import VerificationPage from './components/EmailVerification/VerificationPage';
 import CompanyAndJobTitlePageContainer from './components/CompanyAndJobTitle';
 import CompanyIndexProvider from 'pages/Company/CompanyIndexProvider';
 import CompanyOverviewProvider from 'pages/Company/CompanyOverviewProvider';
-import CompanySalaryWorkTimeProvider from 'pages/Company/CompanyTimeAndSalaryProvider';
+import CompanySalaryWorkTimeProvider from 'pages/Company/CompanySalaryWorkTimeProvider';
 import CompanyInterviewExperiencesProvider from 'pages/Company/CompanyInterviewExperiencesProvider';
 import CompanyWorkExperiencesProvider from 'pages/Company/CompanyWorkExperiencesProvider';
 import JobTitleIndexProvider from 'pages/JobTitle/JobTitleIndexProvider';
 import JobTitleOverviewProvider from 'pages/JobTitle/JobTitleOverviewProvider';
-import JobTitleSalaryWorkTimeProvider from 'pages/JobTitle/JobTitleTimeAndSalaryProvider';
+import JobTitleSalaryWorkTimeProvider from 'pages/JobTitle/JobTitleSalaryWorkTimeProvider';
 import JobTitleInterviewExperiencesProvider from 'pages/JobTitle/JobTitleInterviewExperiencesProvider';
 import JobTitleWorkExperiencesProvider from 'pages/JobTitle/JobTitleWorkExperiencesProvider';
 
