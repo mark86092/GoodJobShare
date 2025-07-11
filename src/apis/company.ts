42c560f545028a2ee469a2cb9171a6eceec6df05
Yen-chi Chen <yenchi.chen@yahooinc.com>
2025-07-11 09:09:21 +0000

    update

--- src/apis/company.ts
+++ src/apis/company.ts
@@ -1,11 +1,4 @@
-import R from 'ramda';
-import graphqlClient from 'utils/graphqlClient';
 import DataResultSortOption from 'apis/dataResultSortOption';
-import {
-  experiencePartialGql,
-  workExperiencesPartialGql,
-} from 'graphql/experience';
-import { CompanyWorkExperience } from './queryCompanyWorkExperiences';
 
 export interface Company {
   name: string;
@@ -18,54 +11,3 @@ export type CompanyExperiencesPaginationInput = {
   limit: number;
   sortBy?: DataResultSortOption;
 };
-
-type QueryCompanyWorkExperiencesData = {
-  company:
-    | (Company & {
-        workExperiencesResult: {
-          count: number;
-          workExperiences: CompanyWorkExperience[];
-        };
-      })
-    | null;
-};
-
-const queryCompanyWorkExperiencesGql = /* GraphQL */ `
-  query(
-    $companyName: String!
-    $jobTitle: String
-    $start: Int!
-    $limit: Int!
-    $sortBy: DataResultSortOption
-  ) {
-    company(name: $companyName) {
-      name
-      workExperiencesResult(
-        jobTitle: $jobTitle
-        start: $start
-        limit: $limit
-        sortBy: $sortBy
-      ) {
-        count
-        workExperiences {
-          ${experiencePartialGql}
-          ${workExperiencesPartialGql()}
-        }
-      }
-    }
-  }
-`;
-
-export const queryCompanyWorkExperiences = ({
-  companyName,
-  jobTitle,
-  start,
-  limit,
-  sortBy,
-}: CompanyExperiencesPaginationInput): Promise<
-  QueryCompanyWorkExperiencesData['company']
-> =>
-  graphqlClient<QueryCompanyWorkExperiencesData>({
-    query: queryCompanyWorkExperiencesGql,
-    variables: { companyName, jobTitle, start, limit, sortBy },
-  }).then(R.prop('company'));
