a8fb2e94074fdfb20bec30d82de86028c0e065c3
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-05-05 01:30:09 +0000

    u

--- src/actions/company.ts
+++ src/actions/company.ts
@@ -294,15 +294,7 @@ export const queryCompanySalaryWorkTime = (
     return;
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD:src/actions/company.ts
-  dispatch(setSalaryWorkTime(companyName, toFetching()));
-=======
-  dispatch(setTimeAndSalary(companyName, toFetching(box)));
->>>>>>> upstream/master:src/actions/company.js
-=======
   dispatch(setSalaryWorkTime(companyName, toFetching(box)));
->>>>>>> rewrite-to-ts
 
   try {
     const data = await queryCompanySalaryWorkTimeApi({
@@ -356,15 +348,7 @@ export const queryCompanySalaryWorkTimeStatistics = ({
     return;
   }
 
-<<<<<<< HEAD
-<<<<<<< HEAD:src/actions/company.ts
-  dispatch(setSalaryWorkTimeStatistics(companyName, toFetching()));
-=======
-  dispatch(setTimeAndSalaryStatistics(companyName, toFetching(box)));
->>>>>>> upstream/master:src/actions/company.js
-=======
   dispatch(setSalaryWorkTimeStatistics(companyName, toFetching(box)));
->>>>>>> rewrite-to-ts
 
   try {
     const data = await queryCompanySalaryWorkTimeStatisticsApi({
