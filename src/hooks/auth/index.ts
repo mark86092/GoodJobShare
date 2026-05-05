b87e576d8bce0913f00bf4bba9d5e0c37cc66617
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-05-05 01:33:35 +0000

    u

--- src/hooks/auth/index.ts
+++ src/hooks/auth/index.ts
@@ -3,19 +3,6 @@ import {
   statusSelector,
   userSelector,
   tokenSelector,
-<<<<<<< HEAD
-  getUserEmailStatus,
-} from 'selectors/authSelector';
-import { User } from 'reducers/auth';
-import AUTH_STATUS from 'constants/authStatus';
-
-export const useIsLoggedIn = (): boolean => {
-  const status = useSelector(statusSelector);
-  return status === AUTH_STATUS.CONNECTED;
-};
-export const useAuthUser = (): User | undefined => useSelector(userSelector);
-export const useAuthUserEmailStatus = () => useSelector(getUserEmailStatus);
-=======
 } from 'selectors/authSelector';
 import AuthStatus from 'constants/authStatus';
 
@@ -24,5 +11,4 @@ export const useIsLoggedIn = (): boolean => {
   return status === AuthStatus.CONNECTED;
 };
 export const useAuthUser = () => useSelector(userSelector);
->>>>>>> upstream/master
 export const useToken = () => useSelector(tokenSelector);
