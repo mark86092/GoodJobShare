be1a7461ef4babfb5ff7430cd1f28073bd75344e
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-05-11 01:48:27 +0000

    chore: rewrite to ts components/CompanyAndJobTitle/utils.ts

--- src/components/CompanyAndJobTitle/utils.ts
+++ src/components/CompanyAndJobTitle/utils.ts
@@ -2,36 +2,61 @@ import {
   pageTypeTranslation,
   tabTypeTranslation,
   TabType,
+  PageType,
   generateIndexURL,
   generatePageURL,
   generateTabURL,
 } from 'constants/companyJobTitle';
 
-const generateRootLayer = () => ({
+type BreadcrumbItem = { label: string; to: string };
+
+const generateRootLayer = (): BreadcrumbItem => ({
   label: 'GoodJob',
   to: '/',
 });
 
-const generatePageTypeLayer = ({ pageType }) => ({
+const generatePageTypeLayer = ({
+  pageType,
+}: {
+  pageType: PageType;
+}): BreadcrumbItem => ({
   label: pageTypeTranslation[pageType],
   to: generateIndexURL({ pageType }),
 });
 
-const generatePageNameLayer = ({ pageType, pageName }) => ({
+const generatePageNameLayer = ({
+  pageType,
+  pageName,
+}: {
+  pageType: PageType;
+  pageName: string;
+}): BreadcrumbItem => ({
   label: pageName,
   to: generatePageURL({ pageType, pageName }),
 });
 
-const generateTabTypeLayer = ({ pageType, pageName, tabType }) => ({
+const generateTabTypeLayer = ({
+  pageType,
+  pageName,
+  tabType,
+}: {
+  pageType: PageType;
+  pageName: string;
+  tabType: TabType;
+}): BreadcrumbItem => ({
   label: tabTypeTranslation[tabType],
-  to: generateTabURL({
-    pageType,
-    pageName,
-    tabType,
-  }),
+  to: generateTabURL({ pageType, pageName, tabType }),
 });
 
-export const generateBreadCrumbData = ({ pageType, pageName, tabType }) => {
+export const generateBreadCrumbData = ({
+  pageType,
+  pageName,
+  tabType,
+}: {
+  pageType: PageType;
+  pageName: string;
+  tabType: TabType;
+}): BreadcrumbItem[] => {
   const data = [
     generateRootLayer(),
     generatePageTypeLayer({ pageType }),
