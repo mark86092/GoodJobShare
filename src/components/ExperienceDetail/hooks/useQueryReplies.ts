db36444bfb404496fd4e717542dc157f99902594
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-06-23 02:35:18 +0000

    u

--- src/components/ExperienceDetail/hooks/useQueryReplies.ts
+++ src/components/ExperienceDetail/hooks/useQueryReplies.ts
@@ -1,11 +1,5 @@
 import { useAsyncFn } from 'react-use';
-<<<<<<< HEAD:src/components/ExperienceDetail/hooks/useQueryReplies.ts
 import { AsyncFn } from 'react-use/lib/useAsyncFn';
-=======
-
-import { queryExperienceReplies } from 'apis/experiencesApi';
-import { useToken } from 'hooks/auth';
->>>>>>> upstream/master:src/components/ExperienceDetail/hooks/useQueryReplies.js
 
 import queryExperienceReplies from 'apis/queryExperienceReplies';
 import { Reply } from 'apis/reply';
