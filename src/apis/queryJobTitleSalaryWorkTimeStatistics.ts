90a2b614ccee021ce98d9212dba4eba58fdfd795
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-07-10 13:59:24 +0000

    u

--- src/apis/queryJobTitleSalaryWorkTimeStatistics.ts
+++ src/apis/queryJobTitleSalaryWorkTimeStatistics.ts
@@ -1,22 +1,9 @@
-<<<<<<< HEAD
-import R from 'ramda';
-
-import { SalaryWorkTimeStatistics } from 'apis/salaryWorkTime';
-import { JobTitle } from 'graphql/jobTitle';
-import graphqlClient from 'utils/graphqlClient';
-
-const queryJobTitleSalaryWorkTimeStatisticsQuery = /* GraphQL */ `
-  query($jobTitle: String!) {
-    job_title(name: $jobTitle) {
-      name
-=======
 import { OvertimeStats } from 'apis/salaryWorkTime';
 import graphqlClient from 'utils/graphqlClient';
 
 const queryJobTitleSalaryWorkTimeStatisticsGql = /* GraphQL */ `
   query($jobTitle: String!) {
     job_title(name: $jobTitle) {
->>>>>>> cc889ec0
       salary_work_time_statistics {
         count
         is_overtime_salary_legal_count {
@@ -39,30 +26,14 @@ const queryJobTitleSalaryWorkTimeStatisticsGql = /* GraphQL */ `
   }
 `;
 
-<<<<<<< HEAD
-export type JobTitleSalaryWorkTimeStatistics = JobTitle & {
-  salary_work_time_statistics: SalaryWorkTimeStatistics;
-};
-
-type QueryJobTitleSalaryWorkTimeStatisticsData = {
-  job_title: JobTitleSalaryWorkTimeStatistics | null;
-=======
 type QueryJobTitleSalaryWorkTimeStatisticsData = {
   job_title: { salary_work_time_statistics: OvertimeStats } | null;
->>>>>>> cc889ec0
 };
 
 const queryJobTitleSalaryWorkTimeStatistics = ({
   jobTitle,
 }: {
   jobTitle: string;
-<<<<<<< HEAD
-}): Promise<QueryJobTitleSalaryWorkTimeStatisticsData['job_title']> =>
-  graphqlClient<QueryJobTitleSalaryWorkTimeStatisticsData>({
-    query: queryJobTitleSalaryWorkTimeStatisticsQuery,
-    variables: { jobTitle },
-  }).then(R.prop('job_title'));
-=======
 }): Promise<OvertimeStats | null> =>
   graphqlClient<QueryJobTitleSalaryWorkTimeStatisticsData>({
     query: queryJobTitleSalaryWorkTimeStatisticsGql,
@@ -70,6 +41,5 @@ const queryJobTitleSalaryWorkTimeStatistics = ({
   }).then(data =>
     data.job_title ? data.job_title.salary_work_time_statistics : null,
   );
->>>>>>> cc889ec0
 
 export default queryJobTitleSalaryWorkTimeStatistics;
