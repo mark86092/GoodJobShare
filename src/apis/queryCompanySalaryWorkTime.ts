604cca5a2d2d87b658f426777443093f8dbc7c6d
Yen-chi Chen <yenchi.chen@yahooinc.com>
2026-07-13 06:52:12 +0000

    u

--- src/apis/queryCompanySalaryWorkTime.ts
+++ src/apis/queryCompanySalaryWorkTime.ts
@@ -1,19 +1,14 @@
 import R from 'ramda';
 
 import {
-<<<<<<< HEAD
-=======
   DataTimeRange,
   ExperienceInYearRange,
->>>>>>> upstream/master
   fragmentSalaryWorkTimeFields,
   SalaryWorkTime,
 } from 'apis/salaryWorkTime';
 import { Company } from 'graphql/company';
 import graphqlClient from 'utils/graphqlClient';
 
-<<<<<<< HEAD
-=======
 const queryCompanySalaryWorkTimeGql = /* GraphQL */ `
   query(
     $companyName: String!
@@ -46,7 +41,6 @@ const queryCompanySalaryWorkTimeGql = /* GraphQL */ `
   ${fragmentSalaryWorkTimeFields}
 `;
 
->>>>>>> upstream/master
 type QueryCompanySalaryWorkTimeData = {
   company:
     | (Company & {
@@ -58,40 +52,11 @@ type QueryCompanySalaryWorkTimeData = {
     | null;
 };
 
-<<<<<<< HEAD
-const queryCompanySalaryWorkTimeGql = /* GraphQL */ `
-  query($companyName: String!, $jobTitle: String, $start: Int!, $limit: Int!) {
-    company(name: $companyName) {
-      name
-      salaryWorkTimesResult(jobTitle: $jobTitle, start: $start, limit: $limit) {
-        count
-        salaryWorkTimes {
-          ...salaryWorkTimeFields
-        }
-      }
-    }
-  }
-  ${fragmentSalaryWorkTimeFields}
-`;
-
-=======
->>>>>>> upstream/master
 const queryCompanySalaryWorkTime = ({
   companyName,
   jobTitle,
   start,
   limit,
-<<<<<<< HEAD
-}: {
-  companyName: string;
-  jobTitle?: string | null;
-  start: number;
-  limit: number;
-}): Promise<QueryCompanySalaryWorkTimeData['company']> =>
-  graphqlClient<QueryCompanySalaryWorkTimeData>({
-    query: queryCompanySalaryWorkTimeGql,
-    variables: { companyName, jobTitle, start, limit },
-=======
   dataTimeRange,
   experienceInYearRange,
   gender,
@@ -118,7 +83,6 @@ const queryCompanySalaryWorkTime = ({
       gender,
       sortBy,
     },
->>>>>>> upstream/master
   }).then(R.prop('company'));
 
 export default queryCompanySalaryWorkTime;
