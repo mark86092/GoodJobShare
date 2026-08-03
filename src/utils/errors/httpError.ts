a184e647bc7fd5941fc04cc040ed7077f8c058a1
Yen-chi Chen <mark86092@gmail.com>
2026-08-03 21:36:51 +0800

    fix

--- src/utils/errors/httpError.ts
+++ src/utils/errors/httpError.ts
@@ -3,10 +3,7 @@ class HttpError extends Error {
 
   constructor(message: string, { statusCode }: { statusCode: number }) {
     super(message);
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream/master
     this.name = 'HttpError';
     this.statusCode = statusCode;
     if (Error.captureStackTrace) {
@@ -16,12 +13,7 @@ class HttpError extends Error {
 }
 
 export default HttpError;
-<<<<<<< HEAD
-export const isHttpError = (error: unknown): error is HttpError =>
-  error instanceof Error && error.name === 'HttpError';
-=======
 
 // See the note in graphqlError.ts on why this compares `name`.
 export const isHttpError = (error: unknown): error is HttpError =>
   error instanceof Object && (error as Error).name === 'HttpError';
->>>>>>> upstream/master
--- src/utils/errors/uiNotFoundError.ts
+++ src/utils/errors/uiNotFoundError.ts
@@ -1,10 +1,7 @@
 class UiNotFoundError extends Error {
   constructor(message?: string) {
     super(message);
-<<<<<<< HEAD
-=======
 
->>>>>>> upstream/master
     this.name = 'UiNotFoundError';
     if (Error.captureStackTrace) {
       Error.captureStackTrace(this, UiNotFoundError);
@@ -13,12 +10,7 @@ class UiNotFoundError extends Error {
 }
 
 export default UiNotFoundError;
-<<<<<<< HEAD
-export const isUiNotFoundError = (error: unknown): error is UiNotFoundError =>
-  error instanceof Error && error.name === 'UiNotFoundError';
-=======
 
 // See the note in graphqlError.ts on why this compares `name`.
 export const isUiNotFoundError = (error: unknown): error is UiNotFoundError =>
   error instanceof Object && (error as Error).name === 'UiNotFoundError';
->>>>>>> upstream/master
